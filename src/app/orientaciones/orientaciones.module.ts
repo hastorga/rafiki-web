32b8df66e888b478aeac65badf3060b651abde42
Héctor Astorga <hm.astorga@gmail.com>
2018-02-13 01:39:17 -0300

    feat: se importan nuevas rutas y RouterModule

--- src/app/orientaciones/orientaciones.module.ts
+++ src/app/orientaciones/orientaciones.module.ts
@@ -4,11 +4,24 @@ import { CrearOrientacionComponent } from './crear-orientacion/crear-orientacion
 import { ListaOrientacionesComponent } from './lista-orientaciones/lista-orientaciones.component';
 import { DetalleOrientacionComponent } from './detalle-orientacion/detalle-orientacion.component';
 import { ModificarOrientacionComponent } from './modificar-orientacion/modificar-orientacion.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  declarations: [CrearOrientacionComponent, ListaOrientacionesComponent, DetalleOrientacionComponent, ModificarOrientacionComponent]
+  declarations: [CrearOrientacionComponent, ListaOrientacionesComponent, DetalleOrientacionComponent, ModificarOrientacionComponent],
+  exports:[
+    ListaOrientacionesComponent,
+    CrearOrientacionComponent,
+    DetalleOrientacionComponent,
+    ModificarOrientacionComponent
+  ]
+
 })
 export class OrientacionesModule { }
