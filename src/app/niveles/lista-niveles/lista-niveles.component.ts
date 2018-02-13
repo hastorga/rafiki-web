d343e3a86f76a0d1cd12b8ddc24356a3ee62f522
Héctor Astorga <hm.astorga@gmail.com>
2018-02-13 15:36:05 -0300

    feat: se importan las animaciones para la lista de niveles

--- src/app/niveles/lista-niveles/lista-niveles.component.ts
+++ src/app/niveles/lista-niveles/lista-niveles.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Routes } from '@angular/router';
-
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { PipeTransform, Pipe } from '@angular/core';
 
 @Component({
   selector: 'app-lista-niveles',
   templateUrl: './lista-niveles.component.html',
-  styleUrls: ['./lista-niveles.component.css']
+  styleUrls: ['./lista-niveles.component.css'],
+  animations: [
+    trigger('animation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('200ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+          ,
+        query(':leave', stagger('200ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class ListaNivelesComponent implements OnInit {
   domain: String = 'http://localhost:10010';
