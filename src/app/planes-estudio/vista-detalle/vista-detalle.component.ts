10a9f75283ba81005a38bc59ec154fe48348e81b
Héctor Astorga <hm.astorga@gmail.com>
2018-03-02 02:08:09 -0300

    feat: se implementan métodos CRUD para los niveles

---
+++ src/app/planes-estudio/vista-detalle/vista-detalle.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Routes } from '@angular/router';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-detalle',
+  templateUrl: './vista-detalle.component.html',
+  styleUrls: ['./vista-detalle.component.css'],
+  animations: [
+    trigger('animation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('200ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+          ,
+        query(':leave', stagger('200ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
+})
+export class VistaDetalleComponent implements OnInit {
+  domain: String = 'http://localhost:10010';
+  nivel: any;
+
+  constructor(private HttpClient: HttpClient) { }
+
+  getNiveles() {
+    this.HttpClient.get(`${this.domain}/niveles`).subscribe(data => {
+      console.log(data);
+      this.nivel = data;
+    });
+  }
+
+  deleteNivel(id) {
+    this.HttpClient.delete(`${this.domain}/niveles/${id}`).subscribe(data => {
+    this.ngOnInit();
+    });
+    
+
+  }
+
+  ngOnInit() {
+    this.getNiveles();
+  }
+}
