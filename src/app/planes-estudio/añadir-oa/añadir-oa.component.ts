efab8dedade1f316eaf5824019ae04dc12679c2d
Héctor Astorga <hm.astorga@gmail.com>
2018-03-04 23:58:09 -0300

    feat: se agrega un estado boolean para los OAs a guardar.

--- src/app/planes-estudio/añadir-oa/añadir-oa.component.ts
+++ src/app/planes-estudio/añadir-oa/añadir-oa.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-añadir-oa',
   templateUrl: './añadir-oa.component.html',
   styleUrls: ['./añadir-oa.component.css']
 })
 export class AñadirOaComponent implements OnInit {
+  private guardar_objetivo: boolean = true;
 
   constructor() { }
 
